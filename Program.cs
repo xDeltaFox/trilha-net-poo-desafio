3f6762ab59dcdc564538e3bf6a492cb7a31ac0be
xDeltaFox <Israelbz25@gmail.com>
2024-01-04 18:20:28 +0000

    Desafio concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+
+Smartphone Nokia = new Nokia("938474574", "Lumia 550", "16432", 8);
+Nokia.Ligar();
+Nokia.InstalarAplicativo("Telegram");
+
+
+Smartphone Iphone = new Iphone("935434564", "14 Pro Max", "3873564", 12);
+Iphone.ReceberLigacao();
+Iphone.InstalarAplicativo("Whatsapp");
